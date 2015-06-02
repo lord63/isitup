ba7e519a099f349adc556415ce52a26a24c20b46
lord63 <lord63.j@gmail.com>
2015-06-02 20:18:09 +0800

    Handle exception
    Thanks to @svisser

--- isitup/main.py
+++ isitup/main.py
@@ -7,16 +7,20 @@ import requests
 
 
 def check(url):
-    response = requests.get(
-        "https://isitup.org/{0}.json".format(url),
-        headers={'User-Agent': 'https://github.com/lord63/isitup'}
-    ).json()
-    status_code = response["status_code"]
+    try:
+        response = requests.get(
+            "https://isitup.org/{0}.json".format(url),
+            headers={'User-Agent': 'https://github.com/lord63/isitup'})
+    except requests.exceptions.ConnectionError:
+        return ("A network problem(e.g. you're offline; refused connection),"
+                "can't check the site right now.")
+    except requests.exceptions.Timeout:
+        return "The request timed out."
+    status_code = response.json()["status_code"]
     if status_code == 1:
-        time, code, ip = (response['response_time'], response['response_code'],
-                          response['response_ip'])
-        return ("Yay, {0} is up.\nIt took {1} ms for a {2} response code "
-                "with an ip of {3}".format(url, time, code, ip))
+        return ("Yay, {0} is up.\nIt took {1[response_time]} ms "
+                "for a {1[response_code]} response code with "
+                "an ip of {1[response_ip]}".format(url, response.json()))
     if status_code == 2:
         return "{0} seems to be down!".format(url)
     if status_code == 3:
